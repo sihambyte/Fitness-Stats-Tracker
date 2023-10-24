ebb9e86d19db89aae4616eaa015b984918e3b0be
sihambyte <sihamargaw@gmail.com>
2023-10-24 14:02:36 -0700

    Initial Commit

---
+++ Fitness Stats Tracker/Fitness_Stats_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_Stats_TrackerApp.swift
+//  Fitness Stats Tracker
+//
+//  Created by siham on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_Stats_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
