ddb4c61711746a743c0509ae56f7a0dec445817b
sihambyte <sihamargaw@gmail.com>
2023-11-11 22:42:37 -0800

    Added working UI for Sign Up and Log In pages

---
+++ Fitness Stats Tracker/Core/Authentication/View/RegistrationView.swift
@@ -0,0 +1,75 @@
+//
+//  RegistrationView.swift
+//  Fitness Stats Tracker
+//
+//  Created by siham on 11/11/23.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var email = ""
+    @State private var fullname = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+    
+    
+    var body: some View {
+        VStack{
+            Image("Fitness")
+                .resizable()
+                .scaledToFill()
+                .frame(width:100, height:120)
+                .padding(.vertical, 32)
+            
+            VStack(spacing:24){
+                InputView(text: $email, title: "Email Address", placeholder: "name@example.com")
+                .autocapitalization(.none)
+                
+                InputView(text: $fullname, title: "Full Name", placeholder: "Enter your name")
+          
+                InputView(text: $password, title: "Password", placeholder: "Enter your password", isSecureField:true)
+                
+                InputView(text: $confirmPassword, title: "Confirm Password", placeholder: "Re-enter your password", isSecureField:true)
+                
+            }
+            .padding(.horizontal)
+            .padding(.top, 12)
+            
+            Button{
+                print("Sign user up..")
+            } label: {
+                HStack{
+                    Text("SIGN UP")
+                        .fontWeight(.semibold)
+                    Image(systemName: "arrow.right")
+                }
+                .foregroundColor(.white)
+                .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+            }
+            .background(Color(.systemBlue))
+            .cornerRadius(10)
+            .padding(.top, 24)
+            
+            Spacer()
+            
+            Button{
+                dismiss()
+            } label:{
+                HStack(spacing: 3){
+                    Text("Already have an account?")
+                    Text("Sign in")
+                        .fontWeight(.bold)
+                }
+                .font(.system(size: 14))
+            }
+        }
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
---
+++ Fitness Stats Tracker/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  Fitness Stats Tracker
+//
+//  Created by siham on 11/11/23.
+//
+
+import Foundation
